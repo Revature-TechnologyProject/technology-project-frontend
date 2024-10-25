46a4ec06025ce1345cb284332fc2937a4661de58
camrcast <camrcast@gmail.com>
2024-10-25 13:36:06 -0700

    Has issue with search

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -9,22 +9,37 @@ function Search() {
     
     const [error, setError] = useState();
     const [displaySuccess, setDisplaySuccess] = useState(false);
-    const [result, setResult] = useState();
+    const [result, setResult] = useState<any>([]);
+    const [page, setPage] = useState(5);
+    const [postsFound, setPostsFound] = useState<any>([]);
+    const [stillMore, setStillMore] = useState(true);
 
     async function search(tags: string, inclusive: string) {
         try {
-            setResult(undefined);
+            setPage(5);
             if (inclusive !== "1"){
                 inclusive = "0";
             }
             const {Posts} = await fetch("get", `/posts/tags/search?tags=${tags}&inclusive=${inclusive}`);
-            setResult(Posts.map((post: any) => <PostCard post={post} key={post.itemID}/>));
+            setPostsFound(Posts);
             setDisplaySuccess(true);
             setError(undefined);
+            loadMore();
         } catch (err: any) {
             setError(err.error);
         }
     }
+
+    async function loadMore(){
+        if (page > postsFound.length){
+            setPage(postsFound.length);
+            setStillMore(false);
+        }
+        const result = postsFound.slice(0, page);
+        setPage(page+5);
+        setResult(result.map((post: any) => <PostCard post={post} key={post.itemID}/>));
+    }
+
 
     return (
         <main id="search">
@@ -32,6 +47,7 @@ function Search() {
             <SearchForm onSubmit={search} error={error}/>
             {displaySuccess && <h4>Results</h4>}
             {displaySuccess && result}
+            {displaySuccess && stillMore && <button onClick={loadMore}>Load More</button>}
         </main>
     )
 }
