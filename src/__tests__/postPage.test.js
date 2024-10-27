0488ea330b12309d95f598074e713aa4309682e7
Jonathan Her <jhawj1@hotmail.com>
2024-10-27 18:38:09 -0500

    Revert "Fix create post tests"
    This reverts commit d109b1df6d57852913befda9774a0a695849bb67.

--- src/__tests__/postPage.test.js
+++ src/__tests__/postPage.test.js
@@ -1,71 +1,22 @@
-import React, {act} from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import Post from "../pages/Post";
 import fetch from "../utilities/fetch";
 
 jest.mock("../utilities/fetch");
 jest.mock("react-router-dom");
-
-const song = {
-    spotifyId: "id",
-    name: "test",
-    link: "listen here",
-    popularity: 100,
-    image: "image",
-    artists: [
-        {
-            id: "id2",
-            name: "artist",
-            url: "spotify"
-        }
-    ]
-};
-
-const song2 = {
-    spotifyId: "id2",
-    name: "test",
-    link: "listen here",
-    popularity: 100,
-    image: "image",
-    artists: [
-        {
-            id: "id2",
-            name: "artist",
-            url: "spotify"
-        }
-    ]
-};
-
-const song3 = {
-    spotifyId: "id3",
-    name: "test",
-    link: "listen here",
-    popularity: 100,
-    image: "image",
-    artists: [
-        {
-            id: "id2",
-            name: "artist",
-            url: "spotify"
-        }
-    ]
-};
-
-describe("Song Selection of Post Form Page", () => {
-    beforeEach(() => {
-        render(<Post/>);
-    });
-
-    test("renders title for page", () => {
-        const title = screen.getByText("Create a Post");
-
-        expect(title).toBeInTheDocument();
-    });
-
+describe("Post Page", () => {
     test("Renders the song form first", () => {
+        render(<Post/>);
+
         const heading = screen.getByText("Find a Song to Review"); 
+
+        expect(heading).toBeInTheDocument();
+    });
+
+    test("Renders form elements for song, artist, album, genre, year, and search", () => {
+        render(<Post/>);
+
         const searchButton = screen.getByRole("button");
         const song = screen.getByLabelText("Song");
         const artist = screen.getByLabelText("Artist");
@@ -83,141 +34,92 @@ describe("Song Selection of Post Form Page", () => {
         expect(genre).toBeInTheDocument();
         expect(yearStart).toBeInTheDocument();
         expect(yearEnd).toBeInTheDocument();
-        expect(heading).toBeInTheDocument();
     });
 
-    test("Renders song selection UI after the song form is entered in", async () => {
-        const fetchResult = {
-            showMore: "",
-            showPrevious: "",
-            songs: [song, song2, song3]
-        }
+    // test("Renders error if request fails", async () => {
 
-        const searchButton = screen.getByRole("button");
-        const songInput = screen.getByLabelText("Song");
-        fetch.mockResolvedValueOnce(fetchResult);
+    //     render(<Post/>);
+    //     const error = {
+    //         error: "invalid score"
+    //     }
 
-        userEvent.type(songInput, "test");
-        await act(async () => {
-            fireEvent.click(searchButton);
-        });
-        const clearButton = screen.getByText("Clear Song Search");
-        const allButtons = await screen.findAllByRole("button");
-        expect(clearButton).toBeInTheDocument();
-        expect(allButtons.length - 1).toBe(fetchResult.songs.length); // Song selection buttons from the songs minus the clear button
-    });
+    //     fetch.mockRejectedValueOnce(error);
 
-    test("Renders post details form after a song is selected", async () => {
-        const fetchResult = {
-            showMore: "",
-            showPrevious: "",
-            songs: [song, song2, song3]
-        }
+    //     const submitButton = screen.getByRole("button");
 
-        const searchButton = screen.getByRole("button");
-        const songInput = screen.getByLabelText("Song");
-        fetch.mockResolvedValueOnce(fetchResult);
+    //     let errorElement = screen.queryByText(error.error);
+    //     expect(errorElement).toBeFalsy();
 
-        userEvent.type(songInput, "test");
-        await act(async () => {
-            fireEvent.click(searchButton);
-        });
+    //     fireEvent.click(submitButton);
 
-        const allButtons = await screen.findAllByRole("button");
+    //     await waitFor(() => {
+    //         errorElement = screen.queryByText(error.error);
+    //         expect(errorElement).toBeInTheDocument();
+    //     }); 
+    // }); 
 
-        await act(async () => {
-            userEvent.click(allButtons[0]);
-        })
+    // test("Renders success if request succeeds", async () => {
+    //     render(<Post/>);
+    //     const successText = "Created Post. Navigating to post in 3 seconds";
 
-        const submitButton = screen.getByText("Submit");
-        const titleInput = screen.getByLabelText("Title*");
-        const scoreInput = screen.getByLabelText("Review Score*");
-        const descriptionInput = screen.getByLabelText("Description*")
-        const tagsInput = screen.getByLabelText("Tags");
+    //     fetch.mockResolvedValueOnce({message: "something", post: {itemID: "something"}});
 
-        expect(submitButton).toBeInTheDocument();
-        expect(titleInput).toBeInTheDocument();
-        expect(scoreInput).toBeInTheDocument();
-        expect(descriptionInput).toBeInTheDocument();
-        expect(tagsInput).toBeInTheDocument(); 
-    });
-});
+    //     const submitButton = screen.getByRole("button");
 
-describe("Post Details Form Tests", () => {
-    beforeEach(async () => {
+    //     let successElement = screen.queryByText(successText);
+    //     expect(successElement).toBeFalsy();
+
+    //     fireEvent.click(submitButton);
+
+    //     await waitFor(() => {
+    //         successElement = screen.queryByText(successText);
+    //         expect(successElement).toBeInTheDocument();
+    //     });
+    // });
+
+    // test("Renders form elements for title, score, description, tags, and submit", () => {
+    //     render(<Post/>);
+
+    //     const submitButton = screen.getByRole("button");
+    //     const titleInput = screen.getByLabelText("Title*");
+    //     const scoreInput = screen.getByLabelText("Review Score*");
+    //     const descriptionInput = screen.getByLabelText("Description*")
+    //     const tagsInput = screen.getByLabelText("Tags");
+
+
+    //     expect(submitButton).toBeInTheDocument();
+    //     expect(titleInput).toBeInTheDocument();
+    //     expect(scoreInput).toBeInTheDocument();
+    //     expect(descriptionInput).toBeInTheDocument();
+    //     expect(tagsInput).toBeInTheDocument();
+    // });
+
+    // test("Form elements for title, score, description are required", () => {
+    //     render(<Post/>);
+
+    //     const titleInput = screen.getByLabelText("Title*");
+    //     const scoreInput = screen.getByLabelText("Review Score*");
+    //     const descriptionInput = screen.getByLabelText("Description*")
+
+    //     expect(titleInput).toHaveAttribute("required");
+    //     expect(scoreInput).toHaveAttribute("required");
+    //     expect(descriptionInput).toHaveAttribute("required");
+    // });
+
+    // test("Score input must be numeric", () => {
+    //     const post = render(<Post/>);
+        
+
+    //     const scoreInput = screen.getByLabelText("Review Score*");
+
+    //     expect(scoreInput).toHaveAttribute("inputmode", "numeric");
+    // })
+
+    test("renders title for page", () => {
         render(<Post/>);
-        const fetchResult = {
-            showMore: "",
-            showPrevious: "",
-            songs: [song, song2, song3]
-        }
 
-        const searchButton = screen.getByRole("button");
-        const songInput = screen.getByLabelText("Song");
-        fetch.mockResolvedValueOnce(fetchResult);
+        const title = screen.getByText("Create a Post");
 
-        userEvent.type(songInput, "test");
-        await act(async () => {
-            fireEvent.click(searchButton);
-        });
-
-        const allButtons = await screen.findAllByRole("button");
-
-        await act(async () => {
-            userEvent.click(allButtons[0]);
-        })
+        expect(title).toBeInTheDocument();
     });
-
-
-    test("Renders error if request fails", async () => {
-        const error = {
-            error: "invalid score"
-        }
-
-        fetch.mockRejectedValueOnce(error);
-
-        const submitButton = screen.getByText("Submit");
-
-        let errorElement = screen.queryByText(error.error);
-        expect(errorElement).toBeFalsy();
-
-        await act(async () => fireEvent.click(submitButton));
-
-        await waitFor(() => {
-            errorElement = screen.queryByText(error.error);
-            expect(errorElement).toBeInTheDocument();
-        }); 
-    }); 
-
-    test("Renders success if request succeeds", async () => {
-        const successText = "Created Post. Navigating to post in 3 seconds";
-
-        fetch.mockResolvedValueOnce({message: "something", post: {itemID: "something"}});
-
-        const submitButton = screen.getByText("Submit");
-
-        let successElement = screen.queryByText(successText);
-        expect(successElement).toBeFalsy();
-
-        await act(async () => fireEvent.click(submitButton));
-
-        successElement = screen.queryByText(successText);
-        expect(successElement).toBeInTheDocument();
-    });
-
-    test("Form elements for title, score, description are required", () => {
-        const titleInput = screen.getByLabelText("Title*");
-        const scoreInput = screen.getByLabelText("Review Score*");
-        const descriptionInput = screen.getByLabelText("Description*")
-
-        expect(titleInput).toHaveAttribute("required");
-        expect(scoreInput).toHaveAttribute("required");
-        expect(descriptionInput).toHaveAttribute("required");
-    });
-
-    test("Score input must be numeric", () => {
-        const scoreInput = screen.getByLabelText("Review Score*");
-
-        expect(scoreInput).toHaveAttribute("inputmode", "numeric");
-    })
-});
+})
