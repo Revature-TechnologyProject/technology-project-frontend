da229a2a7cb72c636241189f68439a58c8618222
camrcast <152932964+camrcast@users.noreply.github.com>
2024-10-28 06:28:53 -0700

    Replies (#17)
    * Replies working but patch not allowed
    
    * When deleting a comment it disappears

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Register from './pages/Register';
 import { User, UserContext } from './context/userContext';
 import CreatePost from './pages/Post';
 import PostDetails from './components/PostDetails/PostDetails';
+import Reply from './pages/Reply';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/register" element={<Register setUser={setUser}/>}/>
           <Route path="/post" element={<CreatePost/>}/>
           <Route path="/posts/:id" element={<PostDetails/>}/>
+          <Route path="/posts/:id/reply" element={<Reply/>}/>
         </Routes>
       </UserContext.Provider>
     </>
