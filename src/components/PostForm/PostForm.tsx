58804e77494686b854fdd57c5cb357a556930e3e
Jonathan Her <90877801+hubmelco@users.noreply.github.com>
2024-10-28 08:28:27 -0500

    Style song selection (#15)
    * Completed song selection styling
    
    * Immediately navigate to post so user can't spam request
    
    * Fix post form tests (#18)
    
    * Fix post creation tests
    
    * Unref timeouts to get rid of test warning
    
    * CI for frontend
    
    * Test CI test command
    
    * Add trigger filter back for CI

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 
 import "./PostForm.css";
-import fetch from "../../utilities/fetch";
 import { Song } from "../SongForm/SongForm";
 
 export interface PostDetails {
