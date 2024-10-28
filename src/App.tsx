ef65f39f75955e33e737e18144e005d2b5bf60f5
camrcast <152932964+camrcast@users.noreply.github.com>
2024-10-28 07:12:07 -0700

    Homepage (#11)
    * Homepage foundations
    
    * Basic Homepage
    
    * Reversed posts list to show most recent first
    
    * removed unnecessary page and clarified top 5 posts
    
    * Now displays most recent post (relies on timestamp added to backend). Also added likes on post to display likes and dislikes total

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Search from './pages/Search';
 import PostDetails from './components/PostDetails/PostDetails';
 import Reply from './pages/Reply';
 import PostUpdate from './pages/PostUpdate';
-
+import Home from './pages/Home';
 
 function App() {
   const [user, setUser] = useState<User>();
@@ -26,6 +26,7 @@ function App() {
           <Route path="/profile/:id" element={<Profile/>}/>
           <Route path="/register" element={<Register setUser={setUser}/>}/>
           <Route path="/post" element={<CreatePost/>}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="/posts/:id" element={<PostDetails/>}/>
           <Route path="/posts/:id/update" element={<PostUpdate/>}/>
           <Route path="/posts/:id/reply" element={<Reply/>}/>
